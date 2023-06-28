c863ee79e811e061579a860e542d162acb13db54
Ryan May <112563297+ryma2fhir@users.noreply.github.com>
2023-06-28 14:40:04 +0100

    Update NamingSystemChecker.py

--- NamingSystemChecker.py
+++ NamingSystemChecker.py
@@ -21,9 +21,9 @@ for path in paths:
         '''Check files are in correct folder '''
         if path == 'structuredefinitions' and (file.endswith("Example.xml") or (not file.startswith('Extension') and not file.startswith('UKCore'))):
             print("The file '"+file+"' has either an incorrect prefix or in the wrong folder '"+path+"'.")
-        if path == 'valuesets' and file.split('-')[0]!='ValueSet':
+        if path == 'valuesets' and not file.startswith('ValueSet'):
             print("The file '"+file+"' has either an incorrect prefix or in the wrong folder '"+path+"'.")  
-        if path == 'codesystems' and file.split('-')[0]!='CodeSystem':
+        if path == 'codesystems' and not file.startswith('CodeSystem'):
             print("The file '"+file+"' has either an incorrect prefix or in the wrong folder '"+path+"'.") 
 
         '''check for missing elements'''
